f9d8635b766d3bc5a789f296e5d672347ac66422
Priyank Bardolia <bardoliapriyank@gmail.com>
2022-03-28 03:43:27 -0700

    Hello World Program

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,13 @@
+// public is the access modifier
+// void means that we don't want our method to return anything
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
+}
+// Java looks for the main method while running the code
+// It is the entry point of any java method
+
+// There are many data types in Java. In total there are 8 primitive data types in Java:
+// int, float, char, boolean, double, short, long, short
