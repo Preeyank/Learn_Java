4b05467f4ea5e9cec993f602bc9bfe2aaf6e9c6b
Priyank Bardolia <bardoliapriyank@gmail.com>
2022-03-28 17:40:25 -0700

    Description of what Float and Double do

---
+++ FloatandDouble/src/academy/learnprogramming/Main.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+       double pounds = 2;
+       double kilograms = pounds * 0.45359237;
+
+       System.out.println("Kilograms: "  +kilograms);
+
+    }
+}
